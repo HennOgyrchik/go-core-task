00ea80f6f9f00b0efcaff7a7de6fae038f9da4a3
Henn <HennOgyrchik@gmail.com>
2024-12-23 13:31:59 +0300

    task 2

--- 2/main2_test.go
+++ 2/main2_test.go
@@ -1,1 +1,131 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sliceExample(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{arr: []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+			want: []int{2, 4, 6, 8},
+		},
+		{
+			name: "2",
+			args: args{arr: []int{1, 3, 5, 7, 9}},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sliceExample(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sliceExample() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_addElements(t *testing.T) {
+	type args struct {
+		arr []int
+		x   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				arr: []int{1, 2, 3, 4, 5},
+				x:   105,
+			},
+			want: []int{1, 2, 3, 4, 5, 105},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addElements(tt.args.arr, tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_copySlice(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{arr: []int{1, 2, 3, 4, 5}},
+			want: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := copySlice(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("copySlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_removeElement(t *testing.T) {
+	type args struct {
+		arr []int
+		i   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				arr: []int{1, 2, 3, 4, 5},
+				i:   2,
+			},
+			want: []int{1, 2, 4, 5},
+		},
+		{
+			name: "2",
+			args: args{
+				arr: []int{1, 2, 3, 4, 5},
+				i:   -5,
+			},
+			want: []int{},
+		},
+		{
+			name: "3",
+			args: args{
+				arr: []int{1, 2, 3, 4, 5},
+				i:   10,
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeElement(tt.args.arr, tt.args.i); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- 2/main_2.go
+++ 2/main_2.go
@@ -1,8 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"slices"
+	"time"
+)
 
 func main() {
-	fmt.Println("task 2")
-	
+
+	originalSlice := newRandomSlice(0, 10, 10)
+	fmt.Println("1. ", originalSlice)
+
+	tmp := sliceExample(originalSlice)
+	fmt.Printf("2.  %v, %p -> %p\n", tmp, originalSlice, tmp)
+
+	tmp = addElements(originalSlice, 55)
+	fmt.Printf("3.  %v, %p -> %p\n", tmp, originalSlice, tmp)
+
+	tmp = copySlice(originalSlice)
+	fmt.Printf("4. %v, %p -> %p\n", tmp, originalSlice, tmp)
+
+	tmp = removeElement(originalSlice, 9)
+	fmt.Printf("5. %v, %p -> %p\n", tmp, originalSlice, tmp)
+}
+
+func newRandomSlice(from, to, count int) []int {
+	result := make([]int, count)
+
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < count; i++ {
+		result[i] = rand.Intn(to-from) + from
+	}
+
+	return result
+}
+
+func sliceExample(arr []int) []int {
+
+	result := []int{}
+
+	for _, a := range arr {
+		if a%2 == 0 {
+			result = append(result, a)
+		}
+	}
+
+	return result
+}
+
+func addElements(arr []int, x int) []int {
+	result := make([]int, 0, len(arr)+1)
+
+	return append(append(result, arr...), x)
+}
+
+func copySlice(arr []int) []int {
+	return slices.Clone(arr)
+}
+
+func removeElement(arr []int, i int) []int {
+	if 0 > i || i >= len(arr) {
+		return []int{}
+	}
+
+	result := make([]int, 0, len(arr)-1)
+
+	return append(append(result, arr[:i]...), arr[i+1:]...)
 }
