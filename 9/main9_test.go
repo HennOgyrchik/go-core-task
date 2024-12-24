0208f0ec9563dc118767a168b7312dcbeec096a2
Henn <HennOgyrchik@gmail.com>
2024-12-24 15:33:42 +0300

    task 9

--- 9/main9_test.go
+++ 9/main9_test.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_conveyer(t *testing.T) {
+	tests := []struct {
+		name string
+
+		data []uint8
+		want []float64
+	}{
+		{
+			name: "test",
+			data: []uint8{2, 3, 4, 5},
+			want: []float64{8, 27, 64, 125},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			in := make(chan uint8)
+			out := conveyer(in)
+
+			go func() {
+				for _, d := range tt.data {
+					in <- d
+				}
+				close(in)
+			}()
+
+			var got []float64
+			for v := range out {
+				got = append(got, v)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("conveyer() = %v, want %v", got, tt.want)
+			}
+
+		})
+	}
+}
--- 9/main_9.go
+++ 9/main_9.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	f := make(chan uint8)
+	s := conveyer(f)
+
+	go func() {
+		for i := 2; i < 5; i++ {
+			f <- uint8(i)
+		}
+		close(f)
+	}()
+
+	for v := range s {
+		fmt.Println(v)
+	}
+}
+
+func conveyer(in chan uint8) <-chan float64 {
+	ch := make(chan float64)
+
+	go func() {
+		for a := range in {
+			ch <- math.Pow(float64(a), 3)
+		}
+		close(ch)
+	}()
+
+	return ch
+}
