a16606b6c9960f6b1b648c621a6c3ff63529f843
Henn <HennOgyrchik@gmail.com>
2024-12-24 12:13:00 +0300

    task 5

--- 5/main5_test.go
+++ 5/main5_test.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_intersection(t *testing.T) {
+	type args struct {
+		s1 []int
+		s2 []int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  bool
+		want1 []int
+	}{
+		{
+			name: "test1",
+			args: args{
+				s1: []int{65, 3, 58, 678, 64},
+				s2: []int{64, 2, 3, 43},
+			},
+			want:  true,
+			want1: []int{3, 64},
+		},
+		{
+			name: "test2",
+			args: args{
+				s1: []int{1, 2, 3, 4, 5},
+				s2: []int{6, 7},
+			},
+			want:  false,
+			want1: []int{},
+		},
+		{
+			name: "test2",
+			args: args{
+				s1: []int{5, 9, 7, 21},
+				s2: []int{6, 3, 56, 9, 95},
+			},
+			want:  true,
+			want1: []int{9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := intersection(tt.args.s1, tt.args.s2)
+			if got != tt.want {
+				t.Errorf("intersection() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				fmt.Println(got1, len(got1), cap(got1))
+				fmt.Println(tt.want1, len(tt.want1), cap(tt.want1))
+
+				t.Errorf("intersection() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
--- 5/main_5.go
+++ 5/main_5.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	a := []int{65, 3, 58, 678, 64}
+	b := []int{64, 2, 3, 43}
+
+	fmt.Println(intersection(a, b))
+
+}
+
+func intersection(s1, s2 []int) (bool, []int) {
+	result := make([]int, 0, 0)
+
+	if len(s2) > len(s1) {
+		s1, s2 = s2, s1
+	}
+
+	for _, v := range s1 {
+		if slices.Contains(s2, v) {
+			result = append(result, v)
+		}
+	}
+
+	return len(result) > 0, result
+}
