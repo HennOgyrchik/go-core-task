27eac9f74dbc0ebbb50da512990450d4d90686ef
Henn <HennOgyrchik@gmail.com>
2024-12-24 12:54:19 +0300

    task 6

--- 6/main6_test.go
+++ 6/main6_test.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"context"
+	"testing"
+)
+
+func Test_randGen(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		from int
+		to   int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		count int
+	}{
+
+		{
+			name: "test",
+			args: args{
+				ctx:  context.Background(),
+				from: 0,
+				to:   10,
+			},
+			count: 100,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ch := randGen(tt.args.ctx, tt.args.from, tt.args.to)
+
+			for i := 0; i < tt.count; i++ {
+				if x := <-ch; x < tt.args.from || x >= tt.args.to {
+					t.Errorf("randGen() = %v, want from %v to %v", x, tt.args.from, tt.args.to)
+				}
+			}
+
+		})
+		tt.args.ctx.Done()
+	}
+}
--- 6/main_6.go
+++ 6/main_6.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	randCh := randGen(ctx, 1, 10)
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(<-randCh)
+	}
+
+	cancel()
+
+}
+
+func randGen(ctx context.Context, from, to int) chan int {
+	ch := make(chan int)
+
+	go func() {
+		rand.New(rand.NewSource(time.Now().UnixNano()))
+
+		for {
+			select {
+			case ch <- rand.Intn(to-from) + from:
+			case <-ctx.Done():
+				close(ch)
+				return
+			}
+		}
+	}()
+
+	return ch
+
+}
