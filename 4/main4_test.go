a0df75321bdf831e2dbf2bc4caabcfcd3b553454
Henn <HennOgyrchik@gmail.com>
2024-12-24 11:40:15 +0300

    task 4

--- 4/main4_test.go
+++ 4/main4_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_f(t *testing.T) {
+	type args struct {
+		s1 []string
+		s2 []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "test",
+			args: args{
+				s1: []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"},
+				s2: []string{"banana", "date", "fig"},
+			},
+			want: []string{"apple", "cherry", "43", "lead", "gno1"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := f(tt.args.s1, tt.args.s2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("f() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- 4/main_4.go
+++ 4/main_4.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
+	slice2 := []string{"banana", "date", "fig"}
+
+	fmt.Println(f(slice1, slice2))
+}
+
+func f(s1, s2 []string) []string {
+	result := make([]string, 0, len(s1))
+
+	for _, v := range s1 {
+		if !slices.Contains(s2, v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
