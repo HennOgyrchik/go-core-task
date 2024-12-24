3e0fc808360d6aadaa7c2bbfe4556ec9a4008144
Henn <HennOgyrchik@gmail.com>
2024-12-24 13:43:15 +0300

    task 7

--- 7/main7_test.go
+++ 7/main7_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args[T comparable] struct {
+		arr []chan T
+	}
+	type testCase[T comparable] struct {
+		name string
+		args args[T]
+		want int
+	}
+	tests := []testCase[int]{
+		{
+			name: "test 1",
+			args: struct{ arr []chan int }{arr: []chan int{make(chan int), make(chan int)}},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			for i, ch := range tt.args.arr {
+				go func() {
+					ch <- i
+					close(ch)
+				}()
+			}
+
+			mCh := merge(tt.args.arr)
+
+			count := 0
+
+			for range mCh {
+				count++
+			}
+
+			if tt.want != count {
+				t.Errorf("merge() = %v, want %v", count, tt.want)
+			}
+
+		})
+	}
+}
--- 7/main_7.go
+++ 7/main_7.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	arr := make([]chan int, 0, 10)
+
+	for i := 0; i < cap(arr); i++ {
+
+		ch := make(chan int)
+		arr = append(arr, ch)
+
+		go func(c chan int) {
+			for j := 0; j < 3; j++ {
+				c <- i
+			}
+			close(ch)
+		}(ch)
+
+	}
+
+	m := merge(arr)
+
+	for v := range m {
+		fmt.Printf("%v ", v)
+	}
+
+}
+
+func merge[T comparable](arr []chan T) <-chan T {
+
+	var wg sync.WaitGroup
+	wg.Add(len(arr))
+
+	mainCh := make(chan T)
+
+	for _, ch := range arr {
+		go func() {
+
+			for v := range ch {
+				mainCh <- v
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(mainCh)
+	}()
+
+	return mainCh
+}
